aabbbc443ef87d91e73db80a8754e9d47eaa1f30
JoshuaBatty <joshpbatty@gmail.com>
2020-05-06 16:58:14 +0200

    created small example to replicate cpal winit conlfict

--- examples/beep.rs
+++ examples/beep.rs
@@ -1,20 +1,58 @@
 extern crate anyhow;
 extern crate cpal;
+extern crate winit;
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
-fn main() -> Result<(), anyhow::Error> {
-    let host = cpal::default_host();
-    let device = host
-        .default_output_device()
-        .expect("failed to find a default output device");
-    let config = device.default_output_config()?;
+use winit::{
+    event::{Event, KeyboardInput, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
 
-    match config.sample_format() {
-        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into())?,
-        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into())?,
-        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into())?,
-    }
+fn main() -> Result<(), anyhow::Error> {
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new()
+        .with_title("Press R to start cpal.")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, _| {
+        use winit::event::{ElementState, VirtualKeyCode};
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(virtual_code),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                } => match virtual_code {
+                    VirtualKeyCode::R => {
+                        // Uncommenting lines 36 and 47 allows cpal to launch
+                        // on it's own thread. This solves the crash.
+                        //std::thread::spawn(move|| {
+                            let host = cpal::default_host();
+                            let device = host
+                                .default_output_device()
+                                .expect("failed to find a default output device");
+                            let config = device.default_output_config().unwrap();
+                            match config.sample_format() {
+                                cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()).unwrap(),
+                                cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()).unwrap(),
+                                cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()).unwrap(),
+                            }
+                        //});
+                    }
+                    _ => (),
+                },
+                _ => (),
+            },
+            _ => (),
+        }
+    });
 
     Ok(())
 }
